7b4d9a5e2295f25facf7189d758968cefbe184ad
RandAlMuhanna <Rand.y.almuhanna@gmail.com>
2023-02-07 10:00:36 +0300

    Initial Commit

---
+++ FalconApplication/FalconApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FalconApplicationApp.swift
+//  FalconApplication
+//
+//  Created by Rand AlMuhanna on 16/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct FalconApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
